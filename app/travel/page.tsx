9837ce6b827104ce38e42baa0bec05339b201ae5
Jonah Bard <jonahbard@gmail.com>
2024-08-25 23:15:18 -0400

    black bg

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold mx-8 lg:mx-24 mt-40 mb-5">Travel</h1>
-      <div className="mx-8 lg:mx-24 flex flex-col justify-center space-y-[1.125rem] mb-32">
+    <div className="bg-black">
+      <h1 className="text-4xl text-white font-bold mx-8 lg:mx-24 pt-40 bg-black mb-5">Travel</h1>
+      <div className="mx-8 lg:mx-24 flex flex-col justify-center bg-black space-y-[1.125rem] mb-32">
         <Image src="/amsterdam+florence.png" alt="Amsterdam" width={1500} height={500}/>
         <Image src="/saxon+izmir.png" alt="Railay" width={1500} height={500}/>
         <Image src="/venice+bolzano.png" alt="Izmir"width={1500} height={500} />
