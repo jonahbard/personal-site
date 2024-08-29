71fd5a19fc6514a769d06c15efd41469ffa30a55
Jonah Bard <jonahbard@gmail.com>
2024-08-29 15:51:57 -0400

    added konak+railay pics and made all images higher res

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -4,10 +4,9 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <div className="bg-black">
-      
       <h1 className="text-4xl text-white font-black mx-8 lg:mx-24 pt-24 lg:pt-40 bg-black mb-5 font-title">
-        <Link href="/" className="font-light italic text-white font-title">Jonah Bard </Link>
-        // Travel
+        <Link href="/" className="font-light italic text-gray-200 font-title">Jonah Bard //  </Link>
+        &nbsp;Travel
         </h1>
       <div className="mx-8 lg:mx-24 flex flex-col justify-center bg-black space-y-[0.5rem] pb-32">
       <p className="text-white pt-4 lg:pt-10 font-body">Amsterdam, Netherlands  •  Florence, Italy </p>
@@ -19,7 +18,7 @@ export default function Page() {
           priority
           placeholder="blur"
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          sizes="(max-width: 3000px) 100vw, (max-width: 3000px) 50vw, 33vw"
         />
         <p className="text-white pt-4 lg:pt-10 font-body">Sachsische-Schweiz, Germany  •  Izmir, Turkey </p>
         <Image 
@@ -29,7 +28,7 @@ export default function Page() {
           height={500}
           placeholder="blur"
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          sizes="(max-width: 3000px) 100vw, (max-width: 3000px) 50vw, 33vw"
         />
         <p className="text-white pt-4 lg:pt-10 font-body">Venice, Italy  •  Bolzano, Italy </p>
         <Image 
@@ -39,7 +38,7 @@ export default function Page() {
           height={500}
           placeholder="blur"
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          sizes="(max-width: 3000px) 100vw, (max-width: 3000px) 50vw, 33vw"
         />
         <p className="text-white pt-4 lg:pt-10 font-body">Meteora, Greece  •  Fez, Morocco </p>
         <Image 
@@ -49,8 +48,18 @@ export default function Page() {
           height={500}
           placeholder="blur"
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          sizes="(max-width: 3000px) 100vw, (max-width: 3000px) 50vw, 33vw"
         />
+        <p className="text-white pt-4 lg:pt-10 font-body">Konak, Turkey  •  Railay Beach, Thailand </p>
+        <Image 
+          src="/konak+railay.png" 
+          alt="Konak and Railay" 
+          width={1500} 
+          height={500}
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
+          sizes="(max-width: 3000px) 100vw, (max-width: 3000px) 50vw, 33vw"
+          />
       </div>
     </div>
   )
